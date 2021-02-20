326df0dafdddf8d9108aae711a7523d215e53a1a
とろんぽっと.nara <NAKNAO-nnct@users.noreply.github.com>
2021-02-20 23:18:36 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,45 @@
-$(function() {
-    var fleet = {
-	    Ise: { name: '伊勢改二', flack: 48, rflack: 85 },
-            Hyuga: { name: '日向改二', flack: 46 },
-    };
+// 艦娘データ
+const fleet = {
+	Ise: {
+		name: '伊勢改二',
+		flack: 48,
+		rflack: 85
+	},
+	Hyuga: {
+		name: '日向改二',
+		flack: 46 
+	}
+}
 
-    $(document).on('input', '#name', function(e) {
-	if($('#name').val() == '伊勢改二')
-	{
-		$('#flack').val([fleet.Ise.flack]);
-		if($('#f:checked').val() == "on")
-		{
-			$('#flack').val([fleet.Ise.rflack]);
-		}
-	}
-	else if($('#name').val() == '日向改二')
-	{
-		$('#flack').val([fleet.Hyuga.flack]);
-	}
-    });
+let inputShip = undefined;
+
+$('#name,#f').on('input click', function() {
+    // 入力された艦娘情報の取得
+	getShipInfo();
     
-    
+    if (inputShip === undefined) {
+        return;
+    }
+
+    // 対空に値セット
+    setAir();
 });
+
+// 入力された艦娘名からデータ取得判断
+function getShipInfo() {
+    const name = $('#name').val();
+	for (const ship in fleet) {
+		if (fleet[ship].name === name) {
+			inputShip = fleet[ship];
+            return;
+		}
+	} 
+	inputShip = undefined;
+}
+
+function setAir() {
+    $('#flack').val(inputShip.flack);
+    if ($('#f:checked').prop('checked')) {
+        $('#flack').val(inputShip.rflack);
+    }
+}
