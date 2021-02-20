d198be66cb19902e547aeb58e56ea81a3902c5f6
Nixajin <rein_ixa@yahoo.co.jp>
2021-02-21 01:44:18 +0900

    色々変更

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
 // 艦娘データ
 const fleet = {
     Ise: {
+	name: '伊勢改',
+	flack: 45,
+	rflack: 92,
+	luck:30,
+	rluck:79 
+    },
+    Ise2: {
         name: '伊勢改二',
         flack: 48,
-        rflack: 85
+        rflack: 85,
+	luck: 40,
+	rluck: 92
     },
     Hyuga: {
+	name: '日向改',
+	flack: 45,
+	rflack: 92,
+	luck: 30,
+	rluck: 79
+    },
+    Hyuga2: {
         name: '日向改二',
-        flack: 46
-    }
+        flack: 46,
+	rflack: 84,
+	luck: 40,
+	rluck: 93,
+    },
+    
 }
 
 let inputShip = undefined;
@@ -23,6 +43,11 @@ $('#name,#f').on('input click', function () {
 
     // 対空に値セット
     setAir();
+
+    setLuck();
+
+    console.log($('#name').val());
+
 });
 
 // 入力された艦娘名からデータ取得判断
@@ -42,4 +67,11 @@ function setAir() {
     if ($('#f').prop('checked')) {
         $('#flack').val(inputShip.rflack);
     }
+}
+
+function setLuck() {
+   $('#luck').val(inputShip.luck);
+   if ($('#f').prop('checked')) {
+	$('#luck').val(inputShip.rluck);
+   }
 }
